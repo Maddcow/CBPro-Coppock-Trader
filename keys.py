5ace84cd14a9baf390024d0ac8a1462fca9c3ded
Maddcow <37763928+Maddcow@users.noreply.github.com>
2021-01-19 22:11:16 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,10 @@
+# Sandbox API keys
+SandboxKey = "key"
+SandboxSecret = "secret"
+SandboxPassphrase = "passphrase"
+Sandboxapi_url="https://api-public.sandbox.pro.coinbase.com"
+
+# Defualt Live API keys
+DefualtKey = "key"
+DefualtSecret = "secret"
+DefualtPassphrase = "passphrase"
